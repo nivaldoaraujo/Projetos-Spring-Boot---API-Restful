16823f9bb759f318e45345402554b03e69b78086
nivaldoaraujo <batista_nba@hotmail.com>
2023-10-31 20:18:20 -0300

    aulas 2 e 3 springBoot

---
+++ AULA 2 e 3/Aula Spring Boot - Fase 1/src/main/java/com/nivaldo/aulaspring/AulaspringApplication.java
@@ -0,0 +1,13 @@
+package com.nivaldo.aulaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaspringApplication.class, args);
+	}
+
+}
